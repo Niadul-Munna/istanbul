faa78395f33ddf91de4c972d1c15bb940fcb44a0
niadul-munna <niadulislammunna@gmail.com>
2025-04-30 21:18:39 +0600

    initial navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+import { navMenu } from "@/data/nav-menu";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#1F212E] w-full p-6 flex items-center justify-between">
+      <div>
+        <Link href="/" className="text-2xl font-bold text-white">
+          Istanbul
+        </Link>
+      </div>
+      <div>
+        <ul className="flex gap-6">
+          {navMenu.map((item, index) => (
+            <li className="text-neutral-100" key={index}>
+              <Link href={item.href}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <Button>Sign Up</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
