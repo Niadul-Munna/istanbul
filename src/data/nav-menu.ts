93a20293072159aa1b9dff43cbf5f115e28b860a
niadul-munna <niadulislammunna@gmail.com>
2025-04-30 21:18:54 +0600

    nav menu data

---
+++ src/data/nav-menu.ts
@@ -0,0 +1,24 @@
+import { NavMenu } from "@/lib/types/nav-menu";
+
+export const navMenu: NavMenu[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Mega Menu",
+    href: "/mega-menu",
+  },
+  {
+    name: "Pricing",
+    href: "/pricing",
+  },
+  {
+    name: "Blog",
+    href: "/blog",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
