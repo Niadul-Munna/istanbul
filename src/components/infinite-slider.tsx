61d3754c66339496d73c20bebcf08818fbbd746a
niadul-munna <niadulislammunna@gmail.com>
2025-05-14 23:10:02 +0600

    img to  nextjs image

--- src/components/infinite-slider.tsx
+++ src/components/infinite-slider.tsx
@@ -1,34 +1,35 @@
 import { InfiniteSlider } from "@/components/motion-primitives/infinite-slider";
+import Image from "next/image";
 
 export function InfiniteSliderBasic() {
   return (
     <InfiniteSlider gap={24}>
-      <img
+      <Image
         src="/apple_music_logo.svg"
         alt="Apple Music logo"
         className="h-[120px] w-auto"
       />
-      <img
+      <Image
         src="/chrome_logo.svg"
         alt="Chrome logo"
         className="h-[120px] w-auto"
       />
-      <img
+      <Image
         src="/strava_logo.svg"
         alt="Strava logo"
         className="h-[120px] w-auto"
       />
-      <img
+      <Image
         src="/nintendo_logo.svg"
         alt="Nintendo logo"
         className="h-[120px] w-auto"
       />
-      <img
+      <Image
         src="/jquery_logo.svg"
         alt="Jquery logo"
         className="h-[120px] w-auto"
       />
-      <img
+      <Image
         src="/prada_logo.svg"
         alt="Prada logo"
         className="h-[120px] w-auto"
